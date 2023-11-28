c0f195abad248a8cedf6ea8aa4d53e0a68de1a03
Zach Van Bennekum <zvanbenn@andrew.cmu.edu>
2023-11-28 17:36:21 -0500

    created disease prediction model

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import joblib
 
 app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load('models/trained_model.pkl')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)  # Run the application in debug mode for development
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get user input from form
+    #symptoms = request.form['symptoms']
+
+    # Preprocess input data (if needed)
+    # Make prediction using the loaded model
+    #predicted_illness = model.predict([symptoms])  # Replace [symptoms] with actual data format
+
+    # Pass the predicted illness to the template
+    #return render_template('index.html', prediction=predicted_illness)
+    pass
---
+++ models/buildModel.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
+
+#load in data to df
+data = pd.read_csv('diseaseData.csv')
+
+# Split the data into symptoms and illness
+X = data.iloc[:, :-1]  #symptoms
+y = data.iloc[:, -1]   #illness
+
+# training and testing sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#train model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+#test model
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print(f"Accuracy: {accuracy:.2f}")
+
+#save trained model to models/trainedModel.pk1
+joblib.dump(model, 'models/illness_prediction_model.pkl')
+
+
+
+
+
